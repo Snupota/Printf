85174470246a3d87a44f2d1b239507fd4b094770
mudimurtala <125508314+mudimurtala@users.noreply.github.com>
2023-10-18 15:21:53 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -85,87 +85,87 @@ int print_string(va_list tpes, char bffr[],
  * @sz: Size specifier
  * Return: Number of chars printed
  */
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_prcnt(va_list tpes, char buff[],
+	int flgs, int width, int precision, int sz)
 {
-	UNUSED(types);
-	UNUSED(buffer);
-	UNUSED(flags);
+	UNUSED(tpes);
+	UNUSED(bffr);
+	UNUSED(flgs);
 	UNUSED(width);
 	UNUSED(precision);
-	UNUSED(size);
-	return (write(1, "%%", 1));
+	UNUSED(sz);
+	return (wrte(1, "%%", 1));
 }
 
 /************************* PRINT INT *************************/
 /**
  * print_int - Print int
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @tpes: Lists of arguments
+ * @bffr: Buffer array to handle print
+ * @flgs:  Calculates active flags
  * @width: get width.
  * @precision: Precision specification
- * @size: Size specifier
+ * @sz: Size specifier
  * Return: Number of chars printed
  */
-int print_int(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_int(va_list tpes, char buff[],
+	int flgs, int width, int precision, int sz)
 {
-	int i = BUFF_SIZE - 2;
-	int is_negative = 0;
-	long int n = va_arg(types, long int);
+	int i = BUFF_SZ - 2;
+	int is_neg = 0;
+	long int n = va_arg(tpes, long int);
 	unsigned long int num;
 
-	n = convert_size_number(n, size);
+	n = convert_sz_num(n, sz);
 
 	if (n == 0)
-		buffer[i--] = '0';
+		bffr[i--] = '0';
 
-	buffer[BUFF_SIZE - 1] = '\0';
-	num = (unsigned long int)n;
+	bffr[BUFF_SZ - 1] = '\0';
+	number = (unsigned long int)n;
 
 	if (n < 0)
 	{
-		num = (unsigned long int)((-1) * n);
-		is_negative = 1;
+		number = (unsigned long int)((-1) * n);
+		is_neg = 1;
 	}
 
-	while (num > 0)
+	while (number > 0)
 	{
-		buffer[i--] = (num % 10) + '0';
-		num /= 10;
+		bffr[i--] = (number % 10) + '0';
+		number /= 10;
 	}
 
 	i++;
 
-	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+	return (wrte_num(is_neg, i, bffr, flgs, width, precision, sz));
 }
 
 /************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
- * @types: Lista of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @tpes: Lista of arguments
+ * @bffr: Buffer array to handle print
+ * @flgs:  Calculates active flags
  * @width: get width.
  * @precision: Precision specification
- * @size: Size specifier
+ * @sz: Size specifier
  * Return: Numbers of char printed.
  */
-int print_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
+int print_binary(va_list tpes, char buff[],
+	int flgs, int width, int precision, int sz)
 {
 	unsigned int n, m, i, sum;
 	unsigned int a[32];
-	int count;
+	int tally;
 
-	UNUSED(buffer);
-	UNUSED(flags);
+	UNUSED(bffr);
+	UNUSED(flgs);
 	UNUSED(width);
 	UNUSED(precision);
-	UNUSED(size);
+	UNUSED(sz);
 
-	n = va_arg(types, unsigned int);
+	n = va_arg(tpes, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
 	for (i = 1; i < 32; i++)
@@ -173,17 +173,17 @@ int print_binary(va_list types, char buffer[],
 		m /= 2;
 		a[i] = (n / m) % 2;
 	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	for (i = 0, sum = 0, tally = 0; i < 32; i++)
 	{
 		sum += a[i];
 		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
 
-			write(1, &z, 1);
-			count++;
+			wrte(1, &z, 1);
+			tally++;
 		}
 	}
-	return (count);
+	return (tally);
 }
 
